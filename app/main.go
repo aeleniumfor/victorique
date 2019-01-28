6891e4c9f3eca2311053498fbaa83e9f911cb0cc
fukuda <fukudammk@gmail.com>
2019-01-29 00:12:08 +0900

    Change repository

--- app/main.go
+++ app/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-	s := echo.New()
-	fmt.Println(s)
+
+	tr := &http.Transport{}
+	cli, err := client.NewClient("http://localhost:2375", client.DefaultVersion, &http.Client{Transport: tr}, map[string]string{})
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+
+	for i, container := range containers {
+		fmt.Println(container.Image)
+		fmt.Println(i)
+	}
+
 }

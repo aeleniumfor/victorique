f6c8bc6922c78029d39ef03b1f6341693ba8a062
fukuda <fukudammk@gmail.com>
2019-02-02 01:53:26 +0900

    Container D

--- app/container/containers.go
+++ app/container/containers.go
@@ -1,2 +1,33 @@
 package container
 
+import "fmt"
+
+// Containers type struct
+type Containers struct {
+	host      []string
+	container Container
+}
+
+// ContainersNew is
+func ContainersNew() (c *Containers) {
+	return &Containers{}
+}
+
+// SetHostList is
+func (c *Containers) SetHostList(host []string) {
+	c.host = host
+}
+
+// GetMultiHostContainer is docker ps s
+func (c *Containers) GetMultiHostContainer() {
+	hostname := c.host
+	for i := 0; i < len(hostname); i++ {
+		s := New(hostname[i])
+		fmt.Println(s)
+	}
+}
+
+// 	s.CreateContainer()
+// 	s.RunContainer()
+// 	s.GetContainerInspect()
+// 	s.StopContainer()

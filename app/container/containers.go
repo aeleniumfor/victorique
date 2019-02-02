3ffe63ffbce37f8b88bbcb77a0b9772714f7708c
fukuda <fukudammk@gmail.com>
2019-02-02 22:51:25 +0900

    Add spece

--- app/container/containers.go
+++ app/container/containers.go
@@ -26,16 +26,11 @@ func pset() {
 // GetMultiHostContainer is docker ps s
 func (c *Containers) GetMultiHostContainer() {
 	hostname := c.host
-	finished := make(chan bool)
 	for i := 0; i < len(hostname); i++ {
 		s := New(hostname[i])
 		fmt.Println(s)
 	}
-
-	funcs := []func(){
-		pset(),
-		pset(),
-	}
+	
 }
 
 // 	s.CreateContainer()

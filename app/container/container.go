10d282e3163798383c35614b289f10d2c9494020
fukuda <fukudammk@gmail.com>
2019-03-12 22:40:48 +0900

    Add file common.go

--- app/container/container.go
+++ app/container/container.go
@@ -5,32 +5,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/victorique/app/common"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
-// Containers is managed
-type Containers struct {
-	host      string
-	client    *client.Client
-	Container []Container // Container List
-}
 
-// Container is managed
-type Container struct {
-	Name string
-	ID   string
-	IP   string
-}
 
 // New is manager
-func New(hostname string) (c *Containers) {
+func New(hostname string) (c *common.Containers) {
 	tr := &http.Transport{}
 	cli, err := client.NewClient(hostname, client.DefaultVersion, &http.Client{Transport: tr}, map[string]string{})
 	if err != nil {
 		log.Println(err)
 	}
-	return &Containers{host: hostname, client: cli}
+
+	return &common.Containers{Host: hostname}
 }
 
 // GetContainerList is multi host get container list

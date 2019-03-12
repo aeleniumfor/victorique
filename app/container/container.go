06e9d37bcc18dc42d746fbf18d2fdf7261036937
fukuda <fukudammk@gmail.com>
2019-03-12 23:05:59 +0900

    Change COntainer

--- app/container/container.go
+++ app/container/container.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/victorique/app/common"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	"github.com/victorique/app/common"
 )
 
 // DClient is docker client
@@ -16,27 +16,27 @@ type DClient struct {
 }
 
 // New is manager
-func New(hostname string) (c *common.Containers, client *DClient) {
+func New(hostname string) (dc *DClient) {
 	tr := &http.Transport{}
 	cli, err := client.NewClient(hostname, client.DefaultVersion, &http.Client{Transport: tr}, map[string]string{})
 	if err != nil {
 		log.Println(err)
 	}
-	return &common.Containers{Host: hostname}, &DClient{cli: cli}
+	// &common.Containers{Host: hostname}
+	return &DClient{cli: cli}
 }
 
 // GetContainerList is multi host get container list
-func (cli, c *common.Containers) GetContainerList() *Containers {
-	list, _ := c.client.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+func (dc *DClient) GetContainerList(c *common.Containers) {
+	list, _ := dc.cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
 	for i := 0; i < len(list); i++ {
-		List := Container{
+		List := c.Containers{
 			list[i].Names[0],
 			list[i].ID,
 			list[i].NetworkSettings.Networks["bridge"].IPAddress, // 中身はmap
 		}
 		c.Container = append(c.Container, List)
 	}
-	return c
 }
 
 // GetContainerNameList is Get Containers Name

95705e6ab4594d0d44755d7d996c7f53c8b8ec8d
fukuda <fukudammk@gmail.com>
2019-03-15 23:36:43 +0900

    Refacoring

--- app/container/container.go
+++ app/container/container.go
@@ -10,27 +10,27 @@ import (
 	"github.com/docker/docker/client"
 )
 
-// DClient is docker client
-type DClient struct {
+// DockerClient is docker client
+type DockerClient struct {
 	cli *client.Client
 }
 
 // New is manager
-func New(hostname string) (dc *DClient) {
+func New(hostname string) (dc *DockerClient) {
 	tr := &http.Transport{}
 	cli, err := client.NewClient(hostname, client.DefaultVersion, &http.Client{Transport: tr}, map[string]string{})
 	if err != nil {
 		log.Println(err)
 	}
 	// &common.Containers{Host: hostname}
-	return &DClient{cli: cli}
+	return &DockerClient{cli: cli}
 }
 
 // GetContainerList is multi host get container list
-func (dc *DClient) GetContainerList(c *common.Containers) {
+func (dc *DockerClient) GetContainerList(c *common.Containers) {
 	list, _ := dc.cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
 	for i := 0; i < len(list); i++ {
-		List := c.Containers{
+		List := c.Container{
 			list[i].Names[0],
 			list[i].ID,
 			list[i].NetworkSettings.Networks["bridge"].IPAddress, // 中身はmap

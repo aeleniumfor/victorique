f72628a6c7923abecaaf6132b5e1ce0deadbec13
aeleniumfor <kuzu.kamo@gmail.com>
2019-05-15 01:48:15 +0900

    Cange docker remote api url -> const ver

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -6,12 +6,15 @@ import (
 	"github.com/labstack/echo"
 )
 
-
+const(
+	// ContainerRemote is docker remote api url
+	ContainerRemote string = "http://localhost:2376"
+)
 
 // HndlerGetContainerIDList is GetContainerIDList 
 func HndlerGetContainerIDList() echo.HandlerFunc {
     return func(c echo.Context) error {
-		cli := container.New("http://localhost:2376")
+		cli := container.New(ContainerRemote)
 		idlist := new(common.ResCotainerIDList)
 		idlist.ContainerID = cli.GetContainerIDList()
 		return c.JSON(200,idlist)
@@ -21,7 +24,7 @@ func HndlerGetContainerIDList() echo.HandlerFunc {
 // HndlerGetContainerNameList is GetContainerIDList 
 func HndlerGetContainerNameList() echo.HandlerFunc {
     return func(c echo.Context) error {
-		cli := container.New("http://localhost:2376")
+		cli := container.New(ContainerRemote)
 		namelist := new(common.ResCotainerNameList)
 		namelist.ContainerName = cli.GetContainerNameList()
 		return c.JSON(200,namelist)

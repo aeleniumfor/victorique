ab77a2184f8b8af9e53e8794ddbb16a8d485036c
aeleniumfor <kuzu.kamo@gmail.com>
2019-05-16 19:27:33 +0900

    Run go fmt app/handler/*.go

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -1,60 +1,60 @@
 package handler
 
 import (
+	"github.com/labstack/echo"
 	"github.com/victorique/app/common"
 	"github.com/victorique/app/container"
-	"github.com/labstack/echo"
 )
 
-const(
+const (
 	// ContainerRemote is docker remote api url
 	ContainerRemote string = "http://localhost:2376"
 )
 
-// GetContainerIDList is GetContainerIDList 
+// GetContainerIDList is GetContainerIDList
 func GetContainerIDList() echo.HandlerFunc {
-    return func(c echo.Context) error {
+	return func(c echo.Context) error {
 		cli := container.New(ContainerRemote)
-		
+
 		idlist := new(common.ResCotainerIDList)
 		idlist.ContainerIDList = cli.GetContainerIDList()
-		return c.JSON(200,idlist)
-    }
+		return c.JSON(200, idlist)
+	}
 }
 
-// GetContainerNameList is GetContainerIDList 
+// GetContainerNameList is GetContainerIDList
 func GetContainerNameList() echo.HandlerFunc {
-    return func(c echo.Context) error {
+	return func(c echo.Context) error {
 		cli := container.New(ContainerRemote)
 
 		namelist := new(common.ResCotainerNameList)
 		namelist.ContainerNameList = cli.GetContainerNameList()
-		return c.JSON(200,namelist)
-    }
+		return c.JSON(200, namelist)
+	}
 }
 
 // ContainerInfoFromID is ContainerID inspect handler
 func ContainerInfoFromID() echo.HandlerFunc {
-    return func(c echo.Context) error {
+	return func(c echo.Context) error {
 		cli := container.New(ContainerRemote)
 
 		containerID := c.Param("id")
-		containerJSON :=new(common.Container)		
+		containerJSON := new(common.Container)
 		containerJSON = cli.GetContainerInfoFromID(containerID)
-		return c.JSON(200,containerJSON)
-    }
+		return c.JSON(200, containerJSON)
+	}
 }
 
 // ContainerInfoFromName is ContainerID inspect handler
 func ContainerInfoFromName() echo.HandlerFunc {
-    return func(c echo.Context) error {
+	return func(c echo.Context) error {
 		cli := container.New(ContainerRemote)
-		
+
 		containerName := c.Param("name")
-		containerJSON :=new(common.Container)
+		containerJSON := new(common.Container)
 		containerJSON = cli.GetContainerInfoFromName(containerName)
-		return c.JSON(200,containerJSON)
-    }
+		return c.JSON(200, containerJSON)
+	}
 }
 
 // CreateContainer is Create container handler
@@ -63,9 +63,9 @@ func CreateContainer() echo.HandlerFunc {
 		cli := container.New(ContainerRemote)
 
 		containerName := c.Param("name")
-		containerID := cli.CreateContainer(containerName) 
-		containerJSON :=new(common.Container)
+		containerID := cli.CreateContainer(containerName)
+		containerJSON := new(common.Container)
 		containerJSON = cli.GetContainerInfoFromID(containerID)
-		return c.JSON(200,containerJSON)
+		return c.JSON(200, containerJSON)
 	}
 }
